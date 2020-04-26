f7ec25578e56b5fa883f2de51e015bffe3d32e63
Ryosuke Miyake <ryosuke12486@gmail.com>
2020-04-26 12:14:40 +0900

    Add Singleton

---
+++ Singleton.java
@@ -0,0 +1,16 @@
+class RegisterNote {
+    private static RegisterNote registerNote = new RegisterNote();
+
+    private RegisterNote() {
+    }
+
+    public static RegisterNote getInstance() {
+        return registerNote;
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+
+    }
+}
